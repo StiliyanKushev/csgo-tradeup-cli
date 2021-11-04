4f961bfda4581220e3b5b80db4b2c1f4a2270657
Stiliyan Kushev <stiliyankushev@gmail.com>
2021-11-04 22:27:01 +0200

    Added check for missing folders.

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const colors = require('colors');
 const { init, buildDatabase, clearDatabase } = require('./db');
 const { cmdHelp, cmdExit, cmdCheckArgs } = require('./cmd');
 const main = require('./perdiction');
+const fs = require('fs');
+const path = require('path');
 let args = process.argv.slice(2);
 
 init(async (db) => {
+    checkFolders();
     cmdCheckArgs();
 
     if(args.includes('--help')){
@@ -25,3 +28,14 @@ init(async (db) => {
     // exit at the end
     cmdExit();
 });
+
+function checkFolders(){
+    if(!fs.existsSync(path.join(process.cwd(),'./dist')))
+    fs.mkdirSync(path.join(process.cwd(),'./dist'));
+    if(!fs.existsSync(path.join(process.cwd(),'./evals')))
+    fs.mkdirSync(path.join(process.cwd(),'./evals'));
+    if(!fs.existsSync(path.join(process.cwd(),'./results')))
+    fs.mkdirSync(path.join(process.cwd(),'./results'));
+    if(!fs.existsSync(path.join(process.cwd(),'./logs')))
+    fs.mkdirSync(path.join(process.cwd(),'./logs'));
+}
